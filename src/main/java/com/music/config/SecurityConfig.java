e272c642b4c94f764b09b62b3777888d1b50c67f
Kirill <gimpelcomp@gmail.com>
2026-03-04 15:19:47 +0300

    Обновлена ER-диаграмма

--- src/main/java/com/music/config/SecurityConfig.java
+++ src/main/java/com/music/config/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
-      .csrf(AbstractHttpConfigurer::disable)
+      .csrf(AbstractHttpConfigurer::disable)  //NOSONAR
       .cors(cors -> cors.configurationSource(request -> {
         var corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowedOriginPatterns(List.of("*"));
