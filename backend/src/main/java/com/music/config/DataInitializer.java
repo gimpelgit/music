e1dff81db723d33235ec9799e91f30f3ff1c32d9
Kirill <gimpelcomp@gmail.com>
2026-03-13 14:40:02 +0300

    Фикс sonar

--- backend/src/main/java/com/music/config/DataInitializer.java
+++ backend/src/main/java/com/music/config/DataInitializer.java
@@ -1,12 +1,9 @@
 package com.music.config;
 
-import com.music.entity.*;
-import com.music.enumeration.Role;
 import com.music.repository.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.init.ScriptUtils;
 import org.springframework.security.crypto.password.PasswordEncoder;
